4e0d93b91931579c0393da51a23c53560b059e4e
Mohammad ibrahim <mohammadibrahim6454@gmail.com>
2022-10-27 13:58:14 +0600

    adding some data for client site course details section

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app=express();
 const cors=require("cors")
 app.use(cors())
 
-const courses=require("./data/CourseName.json")
+const courses=require("./data/CourseName.json");
+const CourseDetails=require("./data/CourseDetails.json")
 
 
 const port=process.env.PORT || 5000
@@ -15,6 +16,11 @@ app.get("/",(req,res)=>{
 app.get("/courses",(req,res)=>{
     res.send(courses)
   
+})
+app.get("/courseDetails/:id",(req,res)=>{
+    const id =req.params.id
+    const selectedCourse=CourseDetails.find(n=>n.id===id)
+    res.send(selectedCourse)
 })
 app.listen(port,()=>{
     console.log(port)
